5bb66911a3a940421f2a3e756504f5e7f7f13d2a
mustfahussain143 <mustfa.hussain.143@gmail.com>
2026-08-24 02:19:01 +0530

    Update KeyboardService.kt

--- app/src/main/java/com/example/messagedesignerkeyboard/KeyboardService.kt
+++ app/src/main/java/com/example/messagedesignerkeyboard/KeyboardService.kt
@@ -3,17 +3,18 @@ package com.example.messagedesignerkeyboard
 import android.inputmethodservice.InputMethodService
 import android.view.View
 import android.view.inputmethod.InputConnection
-import android.widget.LinearLayout
 import android.widget.Button
+import android.widget.LinearLayout
 
 class KeyboardService : InputMethodService() {
 
     override fun onCreateInputView(): View {
+
         val layout = LinearLayout(this)
         layout.orientation = LinearLayout.VERTICAL
 
-        val row = LinearLayout(this)
-        row.orientation = LinearLayout.HORIZONTAL
+        val row1 = LinearLayout(this)
+        row1.orientation = LinearLayout.HORIZONTAL
 
         val button1 = Button(this)
         button1.text = "Hello"
@@ -23,8 +24,17 @@ class KeyboardService : InputMethodService() {
             inputConnection?.commitText("Hello", 1)
         }
 
-        row.addView(button1)
-        layout.addView(row)
+        val button2 = Button(this)
+        button2.text = "Hi"
+
+        button2.setOnClickListener {
+            currentInputConnection?.commitText("Hi", 1)
+        }
+
+        row1.addView(button1)
+        row1.addView(button2)
+
+        layout.addView(row1)
 
         return layout
     }
